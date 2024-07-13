80788806b51886e40c79db6667eeb777123288d5
yanba9 <yan.balbi@gsuite.iff.edu.br>
2024-07-13 12:23:42 -0300

    Remoção temporaria da função getAllTests()

--- controller/ConjuntoComponenteTesteController.js
+++ controller/ConjuntoComponenteTesteController.js
@@ -1,8 +1,7 @@
 const ConjuntoTesteComponenteModel = require('../models/ConjuntoComponenteTesteModel');
 
 function getAllTests(request, response){
-    const posts = "Componente: motor <br> n° teste: 1<br> corrente: 0.5 A <br> tensao 5v <br> data e hora: 26/06/2024 - 17:32<br>================<br>Componente: misturador <br> n° teste: 2<br> corrente: 0.36 A <br> tensao 9v <br> data e hora: 26/06/2024 - 18:00";
-    
+    //TODO: implementar
     response.send(posts);
 }
 
